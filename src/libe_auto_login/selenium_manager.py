6b27221e0814b364bd49346b0df8aacd800422f3
daa-yama <rudie_1987@yahoo.co.jp>
2025-09-28 10:49:44 +0900

    途中

--- src/libe_auto_login/selenium_manager.py
+++ src/libe_auto_login/selenium_manager.py
@@ -1,5 +1,3 @@
-# ////////
-dl和kjfkl邪wg；k邪h；fh
 from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.webdriver.remote.webelement import WebElement
